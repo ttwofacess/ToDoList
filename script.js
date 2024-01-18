322e42fae6cba9c282fa2cd5b69212cd7e72915f
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-01-18 12:09:44 -0300

    Funcion para setear la fecha.

---
+++ script.js
@@ -0,0 +1,18 @@
+// Info date
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+
+
+// Tasks Container
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+    dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+    dateText.textContent = date.toLocaleString('es', { weekday: 'long' });
+    dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
+    dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
+};
+
+
