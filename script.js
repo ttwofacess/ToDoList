bb3d431cb4460d81e2bc6ee18e5f532c62bdc428
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-01-20 08:02:02 -0300

    Ordenar tareas segun estados.

--- script.js
+++ script.js
@@ -31,4 +31,17 @@ const changeTaskState = event => {
     event.target.classList.toggle('done');
 }
 
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done];
+}
+
+const renderOrderedTasks = () => {
+    order().forEach(el => tasksContainer.appendChild(el))
+}
+
 setDate();
